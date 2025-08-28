18a1161fc1e11c3a42bc29538b4845003a3aff9d
Enno Hyttrek <57143243+ennohyttrek@users.noreply.github.com>
2025-08-28 11:59:24 +0200

    Version: 1.0.1
    - Erste php Elemente in die index.php geschrieben

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Medienprojekt Theme Classic</title>
+    <?php wp_head(); ?>
 </head>
 <body>
-	<h1>Hallo Welt!</h1>
+	<h1><?php the_title(); ?></h1>
+    <?php wp_footer(); ?>
 </body>
 </html>
